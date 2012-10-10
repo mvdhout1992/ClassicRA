4eb316de85b825a525a330b033b22f0d1012d8af
Maikel vd H <ma1kelvdh@gmail.com>
2012-10-10 16:38:11 +0200

    added list of contributors to the AI

--- OpenRA.Mods.RA.Classic/AI/IranAI.cs
+++ OpenRA.Mods.RA.Classic/AI/IranAI.cs
@@ -20,6 +20,11 @@ using System.Collections;
 using OpenRA.Mods.RA.Classic.Air;
 using OpenRA.Mods.RA.Classic.Activities;
 
+
+// Based on BetaAI
+// Contributors: JamesDunne, Earthpig, Mart0258, Mailaender, Chrisforbes, Valkirie
+// 
+
 namespace OpenRA.Mods.RA.Classic.AI
 {
     class IranAIInfo : IBotInfo, ITraitInfo
